e0c3f634d0653868fb1cda21a502b6eb3d72fa41
shijianfeng <jeffer7150@163.com>
2021-06-15 14:42:10 +0800

    first commit

---
+++ entry/src/main/java/com/eholee/harmony_mvvm/MyApplication.java
@@ -0,0 +1,10 @@
+package com.eholee.harmony_mvvm;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
